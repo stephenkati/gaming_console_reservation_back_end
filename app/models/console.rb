20fa0d9f01b2f835b0894f747caade082c3ea15a
naledi-dikgale <naledi22khosi@gmail.com>
2023-08-15 12:32:11 +0200

    Add validations

--- app/models/console.rb
+++ app/models/console.rb
@@ -1,2 +1,4 @@
 class Console < ApplicationRecord
+  validates :name, :purchase_price, :rental_price, :description, :photo, presence: true
+  validates :purchase_price, :rental_price, numericality: { greater_than: 0, integer_only: true}
 end
