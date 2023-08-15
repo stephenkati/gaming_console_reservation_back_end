cfab999c929eb8c1b5a417e4a0c50d5d8e3a773f
yacoubou-seidou <yacoseidou@gmail.com>
2023-08-15 11:00:40 +0100

    Add create user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,15 @@
 class Api::V1::UsersController < ApplicationController
+  def create
+    @user= User.new(signup_params)
+    if @user.save
+      sign_in(@user)
+      render json: @user, status: :created
+    else
+      render json: {errors: @user.errors.full_messages}
+      status: :unprocessable_entity
+  end
+
+  def signup_params
+      params.require(:user).permit(:name, :email, :password)
+  end
 end
