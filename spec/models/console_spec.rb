4cf4639bab0bddb2de4a5017acf93f64dab57ea1
stephenkati <stephenkatuli88@gmail.com>
2023-08-15 15:47:36 +0300

    Update rails helper

---
+++ spec/models/console_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Console, type: :model do
+  describe 'validations' do
+    let(:console) { create(:console) }
+
+    it 'should be valid with correct params' do
+      expect(console).to be_valid
+    end
+  end
+end
