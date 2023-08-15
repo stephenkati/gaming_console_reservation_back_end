ac7a984f880c9f32f2beac7c496cfb1a13bc7130
yacoubou-seidou <yacoseidou@gmail.com>
2023-08-15 11:20:01 +0100

    Working on session and auth_token

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::UsersController < ApplicationController
+class Api::V1::RegistrationsController < Devise::RegistrationsController
   def create
     @user= User.new(signup_params)
     if @user.save
---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,3 @@
+class Api::V1::SessionsController < Devise::SessionsController
+
+end
