4fbda3b0547200cc15e82ade0260d7ec47839857
yacoubou-seidou <yacoseidou@gmail.com>
2023-08-15 10:53:28 +0100

    Add name to users

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::ReservationsController < ApplicationController
+  before_action :authenticate_user!
   def index
     @reservations = current_user.reservations
     render json: @reservations
