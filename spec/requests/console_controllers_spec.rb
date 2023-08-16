d2654ec8fc19544ce1eaac957914c87f6ae2bb2b
naledi-dikgale <naledi22khosi@gmail.com>
2023-08-16 09:12:10 +0200

    Add tests for show and create methods

--- spec/requests/console_controllers_spec.rb
+++ spec/requests/console_controllers_spec.rb
@@ -7,17 +7,57 @@ RSpec.describe Api::V1::ConsolesController, type: :request do
       get api_v1_consoles_path
 
       response.body
-      # puts "body - #{response_body}"
       expect(response.body['data'].length).to eq(4)
       expect(response).to have_http_status(:ok)
     end
   end
 
-  # describe '#show' do
-  # end
+  describe '#show' do
+    let(:console) { create(:console) }
 
-  # describe '#create' do
-  # end
+    it 'returns a specific console' do
+      get api_v1_console_path(console.id)
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'returns a 404 if the console is not found' do
+      get api_v1_console_path(999)
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
+  describe '#create' do
+    let(:valid_attributes) do
+      {
+        console: {
+          name: 'Playstation 4',
+          purchase_price: 299,
+          rental_price: 29,
+          description: 'The best console ever',
+          photo: 'image url'
+        }
+      }
+    end
+
+    let(:invalid_attributes) do
+      {
+        console: {
+          name: 'Playstation 4',
+          purchase_price: 'string'
+        }
+      }
+    end
+
+    it 'creates a console' do
+      post api_v1_consoles_path, params: valid_attributes, as: :json
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'returns a 422 if the console is not created' do
+      post api_v1_consoles_path, params: invalid_attributes, as: :json
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
 
   describe '#delete' do
     let(:console) { create(:console) }
