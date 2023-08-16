32d4cba38297d6a0087544530dce57c2655b607a
Miles Mosweu <tmtmosweu@gmail.com>
2023-08-16 13:59:26 +0200

    Add validation for reserve_date

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,11 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :console
   validates :city, :reserve_date, presence: true
+  validate :reserve_date_cannot_be_in_the_past
+
+  def reserve_date_cannot_be_in_the_past
+    if reserve_date.present? && reserve_date < Date.today
+      errors.add(:reserve_date, "can't be in the past")
+    end
+  end
 end
