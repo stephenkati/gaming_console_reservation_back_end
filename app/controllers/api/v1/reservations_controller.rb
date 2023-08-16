7941457b587d4aa4a74af14b5575d82db7d6364e
yacoubou-seidou <yacoseidou@gmail.com>
2023-08-16 10:57:29 +0100

    fix reservations functionnality

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,13 +1,17 @@
 class Api::V1::ReservationsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:create, :index, :destroy]
   def index
     @reservations = current_user.reservations
     render json: @reservations
   end
 
   def create
-    @reservation = current_user.reservations.build(reservation_params)
-    render json: @reservation
+   @reservation = current_user.reservations.build(reservation_params)
+  if @reservation.save
+    render json: @reservation, status: :created
+  else
+    render json: { errors: @reservation.errors.full_messages }, status: :unprocessable_entity
+  end
   end
 
   def destroy
@@ -19,6 +23,6 @@ class Api::V1::ReservationsController < ApplicationController
   private
 
   def reservation_params
-    params.require(:reservation).permit(:reserve_date, :city, :console)
+    params.require(:reservation).permit(:reserve_date, :city, :console_id)
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
+        include DeviseTokenAuth::Concerns::SetUserByToken
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  respond_to :json
   protected
 
   def configure_permitted_parameters
