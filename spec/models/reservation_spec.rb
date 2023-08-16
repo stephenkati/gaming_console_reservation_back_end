9608ee9d1a9bf030facfdbc380d4e3648ec4a6aa
Miles Mosweu <tmtmosweu@gmail.com>
2023-08-16 14:05:34 +0200

    Add validation for past reserve_date

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -17,5 +17,10 @@ RSpec.describe Reservation, type: :model do
       reservation.reserve_date = nil
       expect(reservation).to be_invalid
     end
+
+    it 'should be invalid if reserve date is in the past' do
+      reservation.reserve_date = Faker::Date.backward(days: 23)
+      expect(reservation).to be_invalid
+    end
   end
 end
