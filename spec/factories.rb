abcee0f84346969e52c067d050997bfb49dd043e
Miles Mosweu <tmtmosweu@gmail.com>
2023-08-16 13:20:34 +0200

    Add user factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,10 @@
 FactoryBot.define do
+  factory :user do
+    username { Faker::Internet.username }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
+  end
+
   factory :console do
     name { Faker::Name.name }
     purchase_price { Faker::Number.number(digits: 4) }
