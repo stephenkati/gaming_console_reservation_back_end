2d5b14be0344faad4b797a6672f2534502429b43
stephenkati <stephenkatuli88@gmail.com>
2023-08-16 09:37:30 +0300

    Write tests for index and delete methods

--- spec/requests/console_controllers_spec.rb
+++ spec/requests/console_controllers_spec.rb
@@ -1,9 +1,35 @@
-RSpec.describe Console, type: :model do
-  describe 'validations' do
+require 'rails_helper'
+
+RSpec.describe Api::V1::ConsolesController, type: :request do
+  describe '#index' do
+    it 'returns a list of consoles' do
+      create_list(:console, 4)
+      get api_v1_consoles_path
+
+      response.body
+      # puts "body - #{response_body}"
+      expect(response.body['data'].length).to eq(4)
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  # describe '#show' do
+  # end
+
+  # describe '#create' do
+  # end
+
+  describe '#delete' do
     let(:console) { create(:console) }
 
-    it 'should be valid with correct params' do
-      expect(console).to be_valid
+    it 'deletes a console' do
+      delete api_v1_console_path(console.id)
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'returns a 404 if the console is not found' do
+      delete api_v1_console_path(999)
+      expect(response).to have_http_status(:not_found)
     end
   end
 end
