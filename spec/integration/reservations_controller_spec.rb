109200f36d01b68df624ca6d90ef439b94bd8b87
stephenkati <stephenkatuli88@gmail.com>
2023-08-17 15:49:18 +0300

    Create reservations documentation

---
+++ spec/integration/reservations_controller_spec.rb
@@ -0,0 +1,82 @@
+require 'swagger_helper'
+
+describe 'Reservations API' do
+  before do
+    @user = User.find_by(username: 'test')
+    if @user.nil?
+      @user = User.create(username: 'john', email: 'john@gmail.com', password: 'password', password_confirmation: 'password')
+    end
+    @user_id = @user.id
+    @auth_headers = @user.create_new_auth_token if @user
+    @auth = @auth_headers['Authorization']
+  end
+
+  path '/api/v1/reservations', headers: @auth do
+    post 'create a reservation' do
+      tags 'Reservations'
+      consumes 'application/json'
+      parameter name: :reservation, in: :body, schema: {
+        type: :object,
+        properties: {
+          user_id: { type: :integer },
+          city: { type: :string },
+          reserve_date: { type: :string },
+          console_id: { type: :integer }
+        },
+        required: %w[user_id city reserve_date console_id]
+      }
+
+      response '201', 'reservation created' do
+        let(:reservation) {
+          Reservation.create(
+            user_id: 1,
+            city: 'New York',
+            reserve_date: '2023-12-12',
+            console_id: 1
+            )
+          }
+        run_test!
+      end
+
+      response '422', 'invalid request' do
+        let(:reservation) { { city: 'foo' } }
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/reservations', headers: @auth do
+    get 'Fetch reservations for a user' do
+      tags 'Reservations'
+      produces 'application/json'
+      let(:user_id) {@user.id}
+      parameter name: :user_id, in: :path, type: :string
+
+      response '200', 'reservations found' do
+        schema type: :array,
+          items: {
+            type: :object,
+            properties: {
+              id: { type: :integer },
+              user_id: { type: :integer },
+              city: { type: :string },
+              reserve_date: { type: :string },
+              console_id: { type: :integer }
+            },
+            required: %w[id user_id city reserve_date console_id]
+          }
+
+        let(:reservation){
+         Reservation.create(
+          user_id: 1,
+          city: 'New York',
+          reserve_date: '2023-12-12',
+          console_id: 1
+        )}
+        run_test!
+      end
+    end
+  end
+end
+
+
