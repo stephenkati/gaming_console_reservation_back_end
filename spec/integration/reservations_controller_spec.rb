3777663ba46cb1bc46471bd30f83a260dbf90ee0
stephenkati <stephenkatuli88@gmail.com>
2023-08-17 16:42:44 +0300

    Update documentation

--- spec/integration/reservations_controller_spec.rb
+++ spec/integration/reservations_controller_spec.rb
@@ -1,5 +1,5 @@
 require 'swagger_helper'
-
+# rubocop:disable Metrics/BlockLength
 describe 'Reservations API' do
   before do
     @user = User.find_by(username: 'test')
@@ -7,15 +7,24 @@ describe 'Reservations API' do
       @user = User.create(username: 'john', email: 'john@gmail.com', password: 'password',
                           password_confirmation: 'password')
     end
-    @user_id = @user.id
     @auth_headers = @user.create_new_auth_token if @user
     @auth = @auth_headers['Authorization']
+    @console = Console.create(
+      name: 'Playstation 5',
+      purchase_price: 10_000,
+      rental_price: 100,
+      description: 'Playstation 5',
+      photo: 'photo url'
+    )
   end
 
-  path '/api/v1/reservations', headers: @auth do
+  path '/api/v1/reservations' do
     post 'create a reservation' do
       tags 'Reservations'
       consumes 'application/json'
+      let(:user_id) { @user.id }
+      parameter name: :user_id, in: :path, type: :string
+      parameter name: 'Authorization', in: :header, type: :string
       parameter name: :reservation, in: :body, schema: {
         type: :object,
         properties: {
@@ -28,30 +37,37 @@ describe 'Reservations API' do
       }
 
       response '201', 'reservation created' do
+        let(:Authorization) { @auth }
+        let(:'Content-Type') { 'application/json' }
         let(:reservation) do
           Reservation.create(
-            user_id: 1,
+            user_id: @user.id,
             city: 'New York',
             reserve_date: '2023-12-12',
-            console_id: 1
+            console_id: @console.id
           )
         end
+
         run_test!
       end
 
       response '422', 'invalid request' do
+        let(:Authorization) { @auth }
+        let(:'Content-Type') { 'application/json' }
         let(:reservation) { { city: 'foo' } }
+
         run_test!
       end
     end
   end
 
-  path '/api/v1/reservations', headers: @auth do
+  path '/api/v1/reservations' do
     get 'Fetch reservations for a user' do
       tags 'Reservations'
       produces 'application/json'
       let(:user_id) { @user.id }
       parameter name: :user_id, in: :path, type: :string
+      parameter name: 'Authorization', in: :header, type: :string
 
       response '200', 'reservations found' do
         schema type: :array,
@@ -67,16 +83,19 @@ describe 'Reservations API' do
                  required: %w[id user_id city reserve_date console_id]
                }
 
+        let(:Authorization) { @auth }
         let(:reservation) do
           Reservation.create(
-            user_id: 1,
+            user_id: @user.id,
             city: 'New York',
             reserve_date: '2023-12-12',
-            console_id: 1
+            console_id: @console.id
           )
         end
+
         run_test!
       end
     end
   end
 end
+# rubocop:enable Metrics/BlockLength
