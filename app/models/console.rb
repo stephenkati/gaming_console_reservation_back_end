5d6f2d47f01c0c8216d8949f3c3e788da8005af4
stephenkati <stephenkatuli88@gmail.com>
2023-08-24 09:43:36 +0300

    Add dependent destroy

--- app/models/console.rb
+++ app/models/console.rb
@@ -1,5 +1,5 @@
 class Console < ApplicationRecord
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
 
   validates :name, :purchase_price, :rental_price, :description, :photo, presence: true
   validates :purchase_price, :rental_price, numericality: { greater_than: 0, integer_only: true }
